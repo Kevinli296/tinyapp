d3a2a54ef61e49998a2bedfb27028b6e0fdb303c
Kevinli296 <kevinli296@outlook.com>
2020-11-13 04:07:33 +0000

    implemented checkMatchingUser function to confirm if URL belongs to a user

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+// Helper functions -------------------------------------
 const getUserByEmail = (email, database) => {
     let user;
     for (const key in database) {
@@ -8,4 +9,54 @@ const getUserByEmail = (email, database) => {
   return user;
 }
 
-module.exports = { getUserByEmail };
+const generateRandomString = () => {
+    let result = '';
+    let alphanumeric = 'ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
+  
+    for (let i = 0; i < 6; i++) {
+      let random = (Math.round(Math.random() * (alphanumeric.length - 1)));
+      result += alphanumeric[random];
+    }
+    return result;
+  };
+
+const urlsForUser = (id) => {
+  let tmp = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      tmp[url] = { longURL: urlDatabase[url].longURL, userID: id };
+    }
+  }
+  return tmp;
+};
+
+const checkMatchingUser = (id, url, database) => {
+    let boolean = false;
+  for (const key in database) {
+    if ((database[key].userID === id) && (database[key].longURL === url)) {
+      boolean = true;
+    }
+  }
+  return boolean;
+}
+
+//Data --------------------------------------------------
+const urlDatabase = {
+    b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
+    i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+  };  
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "1234aa"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser, checkMatchingUser, urlDatabase, users};
